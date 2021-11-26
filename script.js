f1bb083c18f09e86c65247e2c0e028ba774da362
Samantha Tarrice <s.tarrice@gmail.com>
2021-11-26 10:20:04 -0300

    Fix navbar dropdown to have larger text and add larger margin to header when open

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const menu = document.querySelector('.fa-bars');
 const dropdown = document.querySelector('.nav-links');
 const hide = document.querySelector('.hide');
+const header = document.querySelector('header');
 
 document.body.addEventListener('click', function(e) {
   if (e.target === menu) {
     dropdown.classList.toggle('hide');
+    header.classList.toggle('header-big-margin');
   } else {
     dropdown.classList.add('hide');
+    header.classList.remove('header-big-margin');
   } 
 })
 
 document.addEventListener('scroll', function() {
   dropdown.classList.add('hide');
+  header.classList.remove('header-big-margin');
 })
 
 window.addEventListener('load', function() {
   dropdown.classList.add('hide');
+  header.classList.remove('header-big-margin');
 })
 
 window.addEventListener('resize', function() {
   dropdown.classList.add('hide');
+  header.classList.remove('header-big-margin');
 })
